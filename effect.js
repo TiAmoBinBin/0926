edab2373e53ac799ab517aafa7537ffc7b98af73
TiAmoBinBin <116493730+TiAmoBinBin@users.noreply.github.com>
2022-10-26 16:16:51 +0800

    Update effect.js

--- effect.js
+++ effect.js
@@ -6,7 +6,7 @@ $('document').ready(function () {
     var vw;
     $('.pigcake').fadeOut('fast');
     $('.heart').fadeOut('fast');
-    $('.firework2').fadeOut('slow');
+    $('.firework2').fadeOut('fast');
     $(window).resize(function () {
         vw = $(window).width() / 2;
         $('#b1,#b2,#b3,#b4,#b5,#b6,#b7').stop();
@@ -170,13 +170,6 @@ $('document').ready(function () {
         $('.pigcake').fadeIn('slow');
         $(this).fadeOut('slow').delay(3000).promise().done(function () {
             $('#light_candle').fadeIn('slow');
-        });
-    });
-
-    $('#light_candle').click(function () {
-        $('.fuego').fadeIn('slow');
-        $(this).fadeOut('slow').promise().done(function () {
-            $('#wish_message').fadeIn('slow');
         });
     });
 
